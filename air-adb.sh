0d1033c4d43f4c14558bafbf872208bf253fd23c
Felipe Herranz <felhr85@gmail.com>
2019-04-07 15:25:13 +0200

    version with default parameters

--- air-adb.sh
+++ air-adb.sh
@@ -2,7 +2,7 @@
 
 # OS dependant constants
 OSX_ADB_DEFAULT_PATH=/Applications/AndroidSDK/sdk/platform-tools/adb
-LINUX_ADB_DEFAULT_PATH=~/Android/Sdk
+LINUX_ADB_DEFAULT_PATH=~/Android/Sdk/platform-tools/adb
 
 # Default values
 PORT=5555
@@ -42,15 +42,21 @@ function getIP3() {
 }
 
 # Get adb from path or directly from default Android SDK installations
+set -e
 ADB_PATH=$(which adb)
 
 if [[ -x $ADB_PATH ]]; then
     ADB="adb"
 else
     ADB=$(getAdbPath)
+    if [[ ! -x $ADB ]]; then
+        echo "air-adb: adb not found in $ADB"
+        exit 1
+    fi
 fi
 
-$ADB kill-server && adb start-server
+# Restart adb
+$ADB kill-server && $ADB start-server
 
 # Get ip of your mobile device trying different methods if necessary
 IP=$(getIP1 $ADB)
@@ -59,14 +65,24 @@ if [[ -z $IP ]]; then
     if [[ -z $IP ]]; then
         IP=$(getIP3 $ADB)
         if [[ -z $IP ]]; then
+            echo "air-adb: couldn't get any ip directly from the device"
             exit -1
         fi
     fi
 fi
 
-echo $IP
+#Validated IP
+VALID_IP=$(echo $IP | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
+if [[ -z $VALID_IP ]]; then
+    echo "air-adb: $IP invalid ip address"
+    exit 1
+fi
 
 # Adb stuff
+#TODO stop execution when something wrong
 echo $($ADB tcpip $PORT)
-echo $($ADB connect $IP)
-#TODO: List information
+echo $($ADB connect $VALID_IP)
+MODEL=$($ADB devices -l | grep $VALID_IP | awk '{print $4}' | awk -F ':' '{print $2}')
+echo "adb-air: $MODEL connected with ip: $VALID_IP, port: $PORT"
+exit 0
+
